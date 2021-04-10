f8cf885b8c03c91bd20a158611dbab4547d13227
engenegr <evdokimov.ilya@gmail.com>
2021-04-10 15:34:10 +0200

    init commit

---
+++ src/main/scala/fr/acinq/alarmbot/WatchdogSync.scala
@@ -0,0 +1,60 @@
+package fr.acinq.alarmbot
+
+import akka.actor.{Actor, DiagnosticActorLogging}
+import fr.acinq.eclair.Kit
+import fr.acinq.eclair.blockchain.watchdogs.BlockchainWatchdog.DangerousBlocksSkew
+
+import java.net.URLEncoder
+import scalaj.http.Http
+
+trait Messenger {
+  val botApiKey = Config.bot
+  val chatId = Config.chat
+  val connectionTimeout = 1000 * 10 // in milliseconds
+  val readTimeout = 1000 * 10 // in milliseconds
+
+  def sendMessage(message: String): Option[String] = {
+    val url = s"https://api.telegram.org/bot$botApiKey/sendMessage" +
+      s"?chat_id=${URLEncoder.encode(chatId, "UTF-8")}" +
+      s"&parse_mode=MarkdownV2" +
+      s"&text=${URLEncoder.encode(message, "UTF-8")}"
+
+    val response = Http(url)
+      .param("chat_id", URLEncoder.encode(chatId, "UTF-8"))
+      .param("parse_mode", "MarkdownV2")
+      .param("text", URLEncoder.encode(message, "UTF-8"))
+      .asString
+
+    if (response.is2xx) Some(response.body)
+    else None
+  }
+}
+
+class WatchdogSync(kit: Kit) extends Actor with DiagnosticActorLogging with Messenger {
+
+  log.info(s"starting actor")
+  context.system.eventStream.subscribe(channel = classOf[DangerousBlocksSkew], subscriber = self)
+  //context.system.eventStream.subscribe(channel = classOf[NewBlock], subscriber = self)
+  log.info(s"subscribed")
+
+  override def preStart() = {
+    val response: Option[String] = sendMessage("Node *runs*")
+    if(response.nonEmpty) {
+      log.info("alarmbot sent message successfully")
+    } else {
+      log.info("failed to send message on preStart")
+    }
+  }
+
+  override def receive: Receive = {
+    case DangerousBlocksSkew(recentHeaders) =>
+      val response: Option[String] = sendMessage("Alarmbot received *DangerousBlocksSkew*")
+      log.info("alarmbot received DANGEROUS BLOCK SKEW")
+    /*
+    case NewBlock(block) =>
+      val response: Option[String] = sendMessage(
+        "Alarmbot received blockid *%s*".format(Try(block.blockId).getOrElse(ByteVector32(ByteVector.empty))))
+      log.info("message sent. response {}", response)
+   */
+  }
+}
