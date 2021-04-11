cabab9eed55188b032c72d2f9f573d56ab115c45
anton <akumaigorodski@gmail.com>
2021-04-11 17:38:16 +0300

    Clean up AlarmbotPlugin Remove unused variables and logging since eclair does a logging on its own to notify that plugin is used.

--- src/main/scala/fr/acinq/alarmbot/AlarmbotPlugin.scala
+++ src/main/scala/fr/acinq/alarmbot/AlarmbotPlugin.scala
@@ -1,28 +1,14 @@
 package fr.acinq.alarmbot
 
-import akka.actor.{ActorSystem, Props}
-import com.typesafe.config.Config
 import fr.acinq.eclair.{Kit, Plugin, PluginParams, Setup}
 import grizzled.slf4j.Logging
+import akka.actor.Props
 
-import scala.concurrent.ExecutionContextExecutor
 
 class AlarmbotPlugin extends Plugin with Logging {
+  override def onSetup(setup: Setup): Unit = { /* Do nothing for now */ }
 
-  var conf: Config = null
-  var kit: Kit = null
-
-  override def onSetup(setup: Setup): Unit = {
-    logger.info(s"plugin for sending messages to telegram bot")
-  }
-
-  override def onKit(kit: Kit): Unit = {
-    val syncRef = kit.system actorOf Props(classOf[WatchdogSync], kit)
-
-    implicit val executionContext: ExecutionContextExecutor = kit.system.dispatcher
-    implicit val coreActorSystem: ActorSystem = kit.system
-  }
-
+  override def onKit(kit: Kit): Unit = kit.system actorOf Props(classOf[WatchdogSync], kit)
 
   override def params: PluginParams = new PluginParams {
     override def name: String = "AlarmBot"
