aef87a47f26685014684f0865f0cab096a341a86
anton <akumaigorodski@gmail.com>
2021-10-27 15:04:56 +0300

    Use `setup.datadir`, simplify stuff

--- src/main/scala/fr/acinq/alarmbot/AlarmbotPlugin.scala
+++ src/main/scala/fr/acinq/alarmbot/AlarmbotPlugin.scala
@@ -1,8 +1,12 @@
 package fr.acinq.alarmbot
 
 import akka.actor.Props
+import com.typesafe.config.{ConfigFactory, Config => TypesafeConfig}
 import fr.acinq.eclair.{Kit, Plugin, PluginParams, Setup}
 import grizzled.slf4j.Logging
+import net.ceedubs.ficus.Ficus._
+
+import java.io.File
 
 
 class AlarmbotPlugin extends Plugin with Logging {
@@ -11,7 +15,15 @@ class AlarmbotPlugin extends Plugin with Logging {
 
   override def onSetup(setup: Setup): Unit = setupRef = setup
 
-  override def onKit(kit: Kit): Unit = kit.system actorOf Props(classOf[WatchdogSync], kit, setupRef)
+  override def onKit(kit: Kit): Unit = {
+    val resourcesDir: File = new File(setupRef.datadir, "/plugin-resources/alarmbot/")
+    val config: TypesafeConfig = ConfigFactory parseFile new File(resourcesDir, "alarmbot.conf")
+
+    val botApiKey: String = config.as[String]("config.botApiKey")
+    val chatId: String = config.as[String]("config.chatId")
+
+    kit.system actorOf Props(classOf[WatchdogSync], setupRef, botApiKey, chatId)
+  }
 
   override def params: PluginParams = new PluginParams {
     override def name: String = "AlarmBot"
--- src/main/scala/fr/acinq/alarmbot/WatchdogSync.scala
+++ src/main/scala/fr/acinq/alarmbot/WatchdogSync.scala
@@ -1,42 +1,39 @@
 package fr.acinq.alarmbot
 
 import akka.actor.DiagnosticActorLogging
-import com.softwaremill.sttp.{SttpBackend, _}
+import com.softwaremill.sttp._
+import fr.acinq.eclair.Setup
 import fr.acinq.eclair.blockchain.bitcoind.zmq.ZMQActor.{ZMQConnected, ZMQDisconnected, ZMQEvent}
 import fr.acinq.eclair.blockchain.watchdogs.BlockchainWatchdog.DangerousBlocksSkew
 import fr.acinq.eclair.channel.{ChannelClosed, ChannelStateChanged, NORMAL, WAIT_FOR_FUNDING_LOCKED}
-import fr.acinq.eclair.{Kit, Setup}
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
-trait Messenger {
-  import fr.acinq.alarmbot.AlarmBotConfig.{botApiKey, chatId}
 
-  val readTimeout: FiniteDuration = 10.seconds
-
-  val baseUri: Uri = uri"https://api.telegram.org/bot$botApiKey/sendMessage"
-
-  def sendMessage(message: String)(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext): Future[Response[String]] = {
-    val parametrizedUri = baseUri.params("chat_id" -> chatId, "text" -> message, "parse_mode" -> "HTML")
-    sttp.readTimeout(readTimeout).get(parametrizedUri).send.map(identity)
-  }
-}
-
-class WatchdogSync(kit: Kit, setup: Setup) extends DiagnosticActorLogging with Messenger {
+class WatchdogSync(setup: Setup, botApiKey: String, chatId: String) extends DiagnosticActorLogging {
   context.system.eventStream.subscribe(channel = classOf[CustomAlarmBotMessage], subscriber = self)
   context.system.eventStream.subscribe(channel = classOf[DangerousBlocksSkew], subscriber = self)
   context.system.eventStream.subscribe(channel = classOf[ChannelStateChanged], subscriber = self)
   context.system.eventStream.subscribe(channel = classOf[ChannelClosed], subscriber = self)
   context.system.eventStream.subscribe(channel = classOf[ZMQEvent], subscriber = self)
 
-  import setup.{ec, sttpBackend}
+  val readTimeout: FiniteDuration = 10.seconds
+
+  val baseUri: Uri = uri"https://api.telegram.org/bot$botApiKey/sendMessage"
+
+  private def sendMessage(message: String)(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext) = {
+    val parametrizedUri = baseUri.params("chat_id" -> chatId, "text" -> message, "parse_mode" -> "HTML")
+    sttp.readTimeout(readTimeout).get(parametrizedUri).send.map(identity)
+  }
 
   def logReport(tag: String): PartialFunction[Try[Response[String]], Unit] = {
     case Failure(reason) => log.info(s"PLGN AlarmBot, failed to send '$tag', reason: ${reason.getMessage}")
     case Success(response) => log.info(s"PLGN AlarmBot, sent '$tag' successfully, response code=${response.code}, body=${response.body}")
   }
+
+  import setup.{ec, sttpBackend}
 
   override def preStart(): Unit = sendMessage("Node runs").onComplete(logReport("preStart"))
 
