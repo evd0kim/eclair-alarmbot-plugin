9d77c8b91d42f8adbc7a5e349afaa4f2c2becf66
engenegr <evdokimov.ilya@gmail.com>
2022-02-08 20:31:53 +0100

    post request to hedge

--- src/main/scala/fr/acinq/alarmbot/ExternalHedgeClient.scala
+++ src/main/scala/fr/acinq/alarmbot/ExternalHedgeClient.scala
@@ -28,11 +28,15 @@ class ExternalHedgeClient(kit: Kit, setup: Setup, pluginConfig: AlarmBotConfig) 
     case Success(response) => log.info(s"PLGN AlarmBot, execute '$tag' successfully, response code=${response.code}, body=${response.body}")
   }
 
-  override def preStart(): Unit = log.info(s"Launching hedge bot")
+  override def preStart(): Unit = {
+    log.info(s"Launching hedge bot")
+    kolliderClient.checkAvailability().onComplete(logReport("preStart"))
+  }
 
   override def receive: Receive = {
     case msg: ExternalHedgeMessage => {
-      kolliderClient.sendMessage(msg.amount, msg.rate).onComplete(logReport("ZMQDisconnected"))
+      //kolliderClient.sendMessage(msg.amount, msg.rate).onComplete(logReport("ExternalHedgeMessage"))
+      kolliderClient.addPosition(msg.amount, msg.rate).onComplete(logReport("ExternalHedgeMessage"))
     }
   }
 }
--- src/main/scala/fr/acinq/alarmbot/KolliderClient.scala
+++ src/main/scala/fr/acinq/alarmbot/KolliderClient.scala
@@ -1,7 +1,9 @@
 package fr.acinq.alarmbot
 
 import com.softwaremill.sttp._
+import com.softwaremill.sttp.json4s.asJson
 import fr.acinq.eclair.MilliSatoshi
+import fr.acinq.eclair.api.serde.JsonSupport.serialization
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
@@ -11,7 +13,27 @@ class KolliderClient(pluginConfig: AlarmBotConfig) {
   val chatId = pluginConfig.chatId
   val baseUri: Uri = uri"https://api.telegram.org/bot$botApiKey/sendMessage"
 
+  val serviceUri: Uri = uri"http://192.168.2.10:8081"
+
   val readTimeout: FiniteDuration = 10.seconds
+
+  def checkAvailability()(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext): Future[Response[String]] = {
+    val parametrizedUri = serviceUri.path("/state")
+    sttp.readTimeout(readTimeout).get(parametrizedUri).send.map(identity)
+  }
+
+  case class HedgeRequest(sats: Long, rate: Long)
+
+  implicit val hedgeRequestSerializer: BodySerializer[HedgeRequest] = { r: HedgeRequest =>
+    val serialized = s"${r.sats},${r.rate}"
+    StringBody(serialized, "UTF-8")
+  }
+  //HedgeRequest(amount.toLong, rate.toLong)
+  //Map("sats"->amount, "rate"->rate)
+  def addPosition(amount: MilliSatoshi, rate: MilliSatoshi)(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext): Future[Response[String]] = {
+    val htlcApiUri = serviceUri.path("/hedge/htlc")
+    sttp.readTimeout(readTimeout).contentType("application/json").post(htlcApiUri).body(HedgeRequest(amount.toLong, rate.toLong)).send.map(identity)
+  }
 
   def sendMessage(amount: MilliSatoshi, rate: MilliSatoshi)(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext): Future[Response[String]] = {
     val parametrizedUri = baseUri.params("chat_id" -> chatId, "text" -> s"hedging ${amount} ${rate}", "parse_mode" -> "HTML")
