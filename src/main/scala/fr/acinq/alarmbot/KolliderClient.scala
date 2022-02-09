fee7caadbb5f496a397ebd3d1ec6837f3f874592
engenegr <evdokimov.ilya@gmail.com>
2022-02-09 18:07:18 +0100

    kollider client works

--- src/main/scala/fr/acinq/alarmbot/KolliderClient.scala
+++ src/main/scala/fr/acinq/alarmbot/KolliderClient.scala
@@ -2,10 +2,16 @@ package fr.acinq.alarmbot
 
 import com.softwaremill.sttp._
 import com.softwaremill.sttp.json4s._
+import fr.acinq.bitcoin.Satoshi
 import fr.acinq.eclair.MilliSatoshi
 
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
+
+//import org.json4s._
+//import org.json4s.jackson.JsonMethods._
+//import org.json4s.JsonDSL._
+import org.json4s.jackson.Serialization.write
 
 class KolliderClient(pluginConfig: AlarmBotConfig) {
   val botApiKey = pluginConfig.botApiKey
@@ -21,18 +27,24 @@ class KolliderClient(pluginConfig: AlarmBotConfig) {
     sttp.readTimeout(readTimeout).get(parametrizedUri).send.map(identity)
   }
 
-  //case class HedgeRequest(sats: Long, rate: Long)
-  //HedgeRequest(amount.toLong, rate.toLong)
   case class HedgeResponse()
+  case class HedgeRequest(channel_id: String, sats: Long, rate: Long)
 
   def addPosition(amount: MilliSatoshi, rate: MilliSatoshi)(implicit http: SttpBackend[Future, Nothing], ec: ExecutionContext): Future[Response[HedgeResponse]] = {
-    val htlcApiUri = serviceUri.path("/hedge/htlc")
     implicit val serialization = org.json4s.native.Serialization
     implicit val formats = org.json4s.DefaultFormats
+
+    val htlcApiUri = serviceUri.path("/hedge/htlc")
+
+    val hedgeRequest = HedgeRequest(
+      "test",
+      amount.truncateToSatoshi.toLong.abs,
+      rate.truncateToSatoshi.toLong)
+    println(write(hedgeRequest))
     sttp.readTimeout(readTimeout)
       .contentType("application/json")
       .post(htlcApiUri)
-      .body(Map("sats"->amount, "rate"->rate))
+      .body(write(hedgeRequest))
       .response(asJson[HedgeResponse])
       .send()
       .map(identity)
